c7a449f510f5484baba3c4d355f7cf03d43c040a
Ruhaker <lhby2389@gmail.com>
2019-06-07 09:56:26 -0300

    Several superficial modifications on code

--- kernelfuzzy/fuzzysets.py
+++ kernelfuzzy/fuzzysets.py
@@ -5,13 +5,11 @@
 """
 class FuzzySet:
 
-    _elements      = None  # Elements of the set
+    _elements      = None  # Elements of the set 
     _elements_type = None  # Object type of elements
     _mf            = None  # Membership function
     _md            = None  # Membership degrees of each element from set
     _params        = None  # Custom parameters of any function
-
-
 
     def __init__(self, elements, md=None, mf=None, params=None):
         
@@ -29,55 +27,71 @@ class FuzzySet:
             (Type: Object "FuzzySet")
 
         """
-        #types of fuzzy sets (unfortunately python does not support several constructors)
-        #FIRST TYPE: elements and membership degrees (md) are given but not the membership function (mf)
+        
+        # has md, but not mf
         self._elements = elements
         self._elements_type = type(elements[0])
 
         if mf is None:
-            self._md=md
+            self._md = md
 
-        # SECOND TYPE: elements and membership function (mf) are given, then the membership degrees are estimated
-
+        # has mf, md generated
         if md is None:
             self._params = params
             self._mf = mf
             self._md = self._mf(self._elements, *self._params)
 
-    def set_membership_degrees(self,membership_degrees):
-        self._md=membership_degrees
+    def set_md(self, md):
+
+        """
+
+        Set the membership values
+
+        Input:
+            md:       (Type: numpy.array)     membership degrees
+            
+        """
+
+        self._md = md
 
     def get_set(self):
+        
         """
 
         Returns the set
 
         """
+        
         return self._elements
 
     def get_function(self):
+        
         """
 
         Returns the membership function
 
         """
+        
         return self._mf
 
     def get_pair(self):
+        
         """
 
-         Returns the pair (_elements, _md) elements and membership degree
+        Returns the pair (_elements, _md) elements and membership degree
 
         """
 
         return list(zip(self._elements,self._md))
 
     def get_degrees(self):
+
         """
 
         Returns the membership degrees
 
         """
+        
         return self._md
 
     def show_class(self):
@@ -87,11 +101,9 @@ class FuzzySet:
         Print in the stdout the contents of the class, for debugging
 
         """
-
-        print("elements      = ", self._elements)
-        print("type of elements = ", self._elements_type)
-        print("membership degrees            = ", self._md)
-
-        if self._mf is not None:
-            print("membership function           = ", self._mf)
-            print("with parameters       = ", self._params)
+        
+        print("_elements      = ", self._elements)
+        print("_elements_type = ", self._elements_type)
+        print("_md            = ", self._md)
+        print("_mf            = ", self._mf)
+        print("_params        = ", self._params)
--- kernelfuzzy/tests/test_kernels.py
+++ kernelfuzzy/tests/test_kernels.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May 31 08:51:36 2019
-
-@author: jorgeluisguevaradiaz
-"""
 import unittest
 from sklearn.metrics.pairwise import linear_kernel
 import numpy as np
 from  kernelfuzzy.kernels import *
 
-
-
-# test with linear kernel
+# tests with linear kernel
 class TestKernels(unittest.TestCase):
 
-    def test_single(self):#unique observation with unique feature
-        X=3
-        Y=4
+    # unique observation with unique feature
+    def test_single(self):
+        X = 3
+        Y = 4
         #self.assertEqual(my_kernel(X, Y),linear_kernel(X,Y), "Should be equal")
         self.assertTrue((my_kernel(X, Y) == linear_kernel(np.array(X).reshape(-1, 1),np.array(Y).reshape(-1, 1))).all(),)
 
-    def test_tuples(self):#unique observation with multiple features
-        X=[3,3]
-        Y=[4,4]
+    def test_tuples(self):# unique observation with multiple features
+        X = [3,3]
+        Y = [4,4]
         #self.np.array_equal(my_kernel(X, Y),linear_kernel(X,Y), "Should be equal")
         self.assertTrue((my_kernel(X, Y) == linear_kernel(np.array(X).reshape(1, -1),np.array(Y).reshape(1, -1))).all(),"Should be equal")
 
